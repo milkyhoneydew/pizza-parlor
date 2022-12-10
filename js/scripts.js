b6960da04a15de845835b71fd740b1eb630aa0f0
Tanya Prado <tanyaprado@gmail.com>
2022-12-10 12:33:56 -0800

    add submitPizza function, submit eventlistener

--- js/scripts.js
+++ js/scripts.js
@@ -6,8 +6,8 @@ function Pizza() {
   this.price = 0;
 }
 
-Pizza.prototype.addTopping = function(topping) {
-  this.toppings.push(topping);
+Pizza.prototype.addTopping = function(...topping) {
+  this.toppings.push(...topping);
 }
 
 Pizza.prototype.pickSize = function(size) {
@@ -35,3 +35,43 @@ Pizza.prototype.calculatePrice = function() {
     this.price += 12;
   }
 }
+
+//UI Logic
+
+function getSize() {
+  let size = "";
+  let radioButtons = document.querySelectorAll("input[type=radio]");
+  for (i=0; i<radioButtons.length; i++) {
+    if (radioButtons[i].checked) {
+      size = radioButtons[i].id;
+    }
+  }
+  return size;
+}
+
+function getToppings() {
+  let toppings = [];
+  let checkboxes = document.querySelectorAll("input[type=checkbox]");
+  for (i=0; i<checkboxes.length; i++) {
+    if (checkboxes[i].checked === true) {
+      toppings.push(checkboxes[i].id)
+    }
+  }
+  return toppings;
+}
+
+function submitPizza(e) {
+  e.preventDefault();
+  let customPizza = new Pizza();
+  let customSize = getSize();
+  let customToppings = getToppings();
+  customPizza.pickSize(customSize);
+  customPizza.addTopping(...customToppings);
+  customPizza.calculatePrice();
+  let customPrice = customPizza.price;
+  document.querySelector("#price").innerText = customPrice;
+}
+
+window.addEventListener("load", function () {
+  document.querySelector("form").addEventListener("submit", submitPizza);
+})
